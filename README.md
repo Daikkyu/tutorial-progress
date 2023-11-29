13911e0831dae1d4496ce1c6c14b5c4ecec3a096
Daikkyu <lluisye20@gmail.com>
2023-11-29 21:03:46 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tutorial-progress
+
+Hi!
+
+So this repository will be documenting my progress in programming language tutorials.
+
+Feel free to see, I guess.
---
+++ helloworld.js
@@ -0,0 +1,91 @@
+console.log('Hello World');
+
+console.log(3 + 2);
+console.log(3 - 2);
+console.log(3 * 2);
+console.log(3 / 2);
+console.log(3 % 2);
+console.log(3 ** 2);
+
+let variable;
+
+const constant = 1; // Constant...
+
+
+console.log(variable); // Undefined, same as null
+
+console.log(variable == null); // returns true
+
+
+
+/** So everything between '', "" or `` can be a string
+ * 
+ *  The arithmetic operatiors are identical to the ones in Java, except a new one
+ *  console.log(3 ** 2); exponential
+ * 
+ *  Include a script on a .html file using <script src="location-of-the-script.js"></script> either in the body or head
+ *  There can be multiple scripts in one .html
+ *  
+ *  
+ * 
+*/
+
+// Exercises 1st Day
+
+// 1)
+// comments can make code readable
+// 2)
+// Welcome to 30DaysOfJavaScript
+// 3)
+/**
+ * comments can make code readable, 
+ * easy to reuse and informative
+ */
+
+// 4) (I'm not gonna make another js just to create some variables)
+
+let str = 'string';
+let bool = true;
+let undef;
+let n = null;
+
+// 5)
+
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof undef);
+console.log(typeof n);
+
+// 6)
+
+let var1;
+let var2;
+let var3;
+let var4;
+
+// 7)
+
+let var5;
+let var6;
+let var7;
+let var8;
+
+// 8)
+
+let firstName;
+let lastName;
+let maritalStatus;
+let country;
+let age;    
+
+// 9)
+
+let firstName2, lastName2, maritalStatus2, country2, age2;
+
+// 10)
+
+let myAge = 25, yourAge = 30;
+console.log("I am " + myAge + " years old");
+console.log("You are " + yourAge + " years old");
+
+
